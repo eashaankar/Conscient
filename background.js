dd49aba05d1398ff539fbd4b4b7adf3d3d9abc13
Soumya <55809178+Inovators5566@users.noreply.github.com>
2020-09-19 04:56:16 +0530

    background.js added

---
+++ background.js
@@ -0,0 +1,2 @@
+
+console.log('document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.rG0ybd.fT3JUc > div.q2u11 > div.a1GRr > div > div > div > div.I5fjHe.wb61gb).click()');
