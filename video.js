fe87d2d4666f7d5afeabf809989f2381c8e7bc6f
Akshata Bihany <51451161+Akshatabihany@users.noreply.github.com>
2020-09-19 15:52:58 +0530

    video.js file

---
+++ video.js
@@ -0,0 +1,7 @@
+
+if(document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.rG0ybd.LCXT6 > div.q2u11 > div.SfBQ6c > div > div").dataset.isMuted == "false"){
+    document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.rG0ybd.LCXT6 > div.q2u11 > div.SfBQ6c > div > div").click()
+  }
+  
+  document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.rG0ybd.LCXT6 > div.q2u11 > div.SfBQ6c > div > div").style.pointerEvents = "none"
+  
