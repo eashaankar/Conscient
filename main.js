3cfa15f9e0bfa687ae0635c94ef01608786d3c2a
Soumya <55809178+Inovators5566@users.noreply.github.com>
2020-09-20 03:22:46 +0530

    dark mode updated

---
+++ main.js
@@ -0,0 +1,18 @@
+let prviBtn = document.getElementById('prvo');
+let btn = document.querySelector('#prvo');
+btn.addEventListener("click",function(){
+document.body.classList.toggle('colored');
+
+
+});
+
+
+let yo = document.getElementById('hello');
+//let bgo = document.querySelector('#hello');
+btn.addEventListener("click",function(){
+document.body.classList.toggle('doc');
+
+});
+
+
+
